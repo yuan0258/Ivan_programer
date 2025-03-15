75fb665ea485438d443fe08a11d5599db6a416f3
yuan0258 <yuan0258@gmail.com>
2025-03-15 08:44:51 +0800

    Count Characters

--- test3.c
+++ test3.c
@@ -2,12 +2,11 @@
 
 int main(int argc, char *argv[]) {
 	int c = 0;
+	long nc = 0; /* count character in input; 1st version */
 
-	/* Copy input to output; 1st version, crtl + d to exit */
-	c = getchar();
-	while ( c != EOF) {
-		putchar(c);
-		c = getchar();
-	}
+	while (getchar() != EOF)
+		++nc;
+
+	printf("nc is %ld\n", nc);
 
 }
