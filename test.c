c641b56760edc000ba3070ac91cb26e79c35c48a
yuan0258 <yuan0258@gmail.com>
2025-03-15 08:29:39 +0800

    Fahrenheit-Celsius table float version
    This program result is
      0      -17.8
     20       -6.7
     40        4.4
     60       15.6
     80       26.7
    100       37.8
    120       48.9
    140       60.0
    160       71.1
    180       82.2
    200       93.3
    220      104.4
    240      115.6
    260      126.7
    280      137.8
    300      148.9

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table */
+/* print Fahrenheit-Celsius table, floating-point version */
 int main(int argc, char *argv[]) {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;
@@ -11,8 +11,8 @@ int main(int argc, char *argv[]) {
 	
 	fahr = lower;
 	while (fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr - 32.0);
+		printf("%3.0f\t%6.1f\n", fahr, celsius);
 		fahr += step;
 	}
 	printf("hello ivan\n");
